631d43977a5ce41e02ea69f7d8fa9ad81de007f5
Alek Slater <slayte@gmail.com>
2017-05-30 16:20:04 +0800

    Revert "trying to deobjc the lib"
    This reverts commit bcb159d5d7fb28ca80a1928ffab4d11b1fc1f923.

--- Source/SwiftyDropbox/Shared/Handwritten/SwiftyDropbox.h
+++ Source/SwiftyDropbox/Shared/Handwritten/SwiftyDropbox.h
@@ -20,3 +20,5 @@ FOUNDATION_EXPORT double SwiftyDropboxVersionNumber;
 FOUNDATION_EXPORT const unsigned char SwiftyDropboxVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftyDropbox/PublicHeader.h
+
+#import "DBChunkInputStream.h"
